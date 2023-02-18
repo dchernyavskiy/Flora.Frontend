3a2add2f225d2daf2ae55c9a121a460791f73f67
dchernyavskiy <99835818+dchernyavskiy@users.noreply.github.com>
2023-02-18 23:39:53 +0200

    updated base api client

--- src/app/api/client-base.ts
+++ src/app/api/client-base.ts
@@ -1,8 +1,13 @@
 export class ClientBase {
-  protected constructor() {}
+  protected constructor() {
+  }
 
   protected transformOptions(options: any): Promise<any> {
     const authToken = localStorage.getItem('token');
+    options = {
+      ...options,
+      withCredentials: true
+    }
     options.headers = options.headers.append(
       'Authorization',
       'Bearer ' + authToken
